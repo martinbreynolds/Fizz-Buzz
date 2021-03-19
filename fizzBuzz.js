5d78dd273042b662f5ba7cc59aa0014c1cbcd5b4
Martin Reynolds <62625037+martinbreynolds@users.noreply.github.com>
2021-03-19 12:59:42 +0000

    Add Pseudocode

---
+++ fizzBuzz.js
@@ -0,0 +1,18 @@
+//Martin Reynolds - Fizz Buzz Pseudocode
+
+/*
+1. FOR LOOP:
+   Set Initial Counter(number) to 1
+   When Counter(number) reaches 100, stop loop 
+   Increment counter(number) by 1 on each loop
+   
+       IF number Modulas(%) 3 && Modulas(%) 5 is equal to 0 
+            PRINT 'fizzbuzz'
+       3. ELSE IF number Modulas(%) 3 is equal to 0 
+            PRINT 'fizz'
+       4. ELSE IF number Modulas(%) 5 is equal to 0 
+            PRINT 'buzz'
+       5. ELSE 
+            PRINT number
+6. END LOOP          
+*/
